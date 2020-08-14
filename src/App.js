a928f9330472047b8df12244ec132e1a96abe489
steinhakasevs <stein.hakase.vs@gmail.com>
2020-08-14 09:41:09 -0600

    Created Apollo store and router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import { Button } from 'antd'
+import { ApolloProvider } from '@apollo/client'
+import client from './setup/apollo'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Main from './layout/main'
 
 function App () {
   return (
-    <div className='container'>
-      <div className='md:flex bg-cyan rounded-lg p-6'>
-        <img className='h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6' src='avatar.jpg' />
-        <div className='text-center md:text-left'>
-          <h2 className='text-lg res'>Erin Lindford</h2>
-          <div className='text-purple-500'>Product Engineer</div>
-          <div className='text-gray-600'>erinlindford@example.com</div>
-          <div className='text-gray-600'>(555) 765-4321</div>
-          <Button type='primary'>Button</Button>
-        </div>
-      </div>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Main />
+      </Router>
+    </ApolloProvider>
   )
 }
 
