6a1a49dddda8dc328c3c2aff0cb1cb34d5001fdc
steinhakasevs <stein.hakase.vs@gmail.com>
2020-08-14 09:44:15 -0600

    Creted country details card

---
+++ src/components/countryDetailsCard/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Card } from 'antd'
+import { toLocaleString } from '../../utils'
+
+const renderTitle = (title, amount) => {
+  return (
+    <div className='flex justify-between'>
+      <span className='capitalize'>{title}</span>
+      <span>{toLocaleString(amount)}</span>
+    </div>
+  )
+}
+
+const CountryDetailsCard = (props) => {
+  const data = {
+    title: props.title || '',
+    content: props.content || [{
+      title: '',
+      amount: ''
+    }]
+  }
+
+  if (typeof props.title === 'object') {
+    data.title = renderTitle(data.title.title, data.title.amount)
+  }
+
+  return (
+    <Card title={data.title}>
+      {
+        data.content.map(({ title, amount }, i) => (
+          <p key={i}>
+            {title}: {
+              !amount || typeof amount === 'number'
+                ? toLocaleString(amount)
+                : amount
+            }
+          </p>
+        ))
+      }
+    </Card>
+  )
+}
+
+export default CountryDetailsCard
